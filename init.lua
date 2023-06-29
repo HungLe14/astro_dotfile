f23188fd6c9f19283714e9c460524642b3c83fa4
Hung Le (Harry) <lv.hung1410@gmail.com>
2023-06-29 19:25:15 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -60,10 +60,15 @@ return {
       end
     },
     {
-      "tpope/vim-surround",
-      -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-      setup = function() vim.o.timeoutlen = 500 end
-    },
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  },
     {'akinsho/git-conflict.nvim', version = "*", config = true}
     
   },
